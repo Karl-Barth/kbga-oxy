45ac4cfc92d461a3a1426ed21b9e559c8a590aa8
andreas_kraenzle <kraenzle@k-r.ch>
2026-07-07 09:13:27 +0200

    kbga-oxy 1.1.2: breiterer, konfigurierbarer Vorschau-Kontext bei weiteren Vorkommen

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ HERE="$(cd "$(dirname "$0")" && pwd)"
 cd "$HERE"
 
 OXYGEN_DIR="${OXYGEN_DIR:-/Applications/Oxygen XML Editor}"
-VERSION="1.1.1"
+VERSION="1.1.2"
 JAR="lib/kbga-oxy-${VERSION}.jar"
 TARGET="$OXYGEN_DIR/plugins/kbga-oxy"
 
